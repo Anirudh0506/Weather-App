41f2d7aae84d104a36039954271fbdd6194db7d8
Anupam-Bandi <anupam.bandi@ibm.com>
2024-10-20 12:58:14 +0530

    First commit

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="#"
+        > Anirudh Bandi </a
+      >, <a href="https://github.com/Anirudh0506/Weather-App/">Open sourced on Github.</a>
+    </p>
+  </div>
+  );
+}
